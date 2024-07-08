3e5aa851e77caf9fb30a803489f23da34582ea38
Collins Amuhaya <156360284+collinsamuhaya@users.noreply.github.com>
2024-07-08 18:41:36 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd /var/www/html 
+
+cp .env.example .env
+
+sed -i 's/DB_HOST=.*/DB_HOST=mariadb/g' .env 
+sed -i 's/DB_USERNAME=.*/DB_USERNAME=root/g' .env
+sed -i 's/DB_PORT=.*/DB_PORT=3306/g' .env 
+DATABASE_URL
+
+rm -rf Modules/*
+rm composer.lock
+ 
+composer install --no-interaction
+
+composer require mybizna/account --no-interaction
+
+if [ -f /var/www/html/entrypoint-composers.sh ]; then
+    chmod +x /usr/local/bin/entrypoint.sh
+    /bin/sh /var/www/html/entrypoint-composers.sh
+fi
+
+php artisan key:generate
+
+php artisan migrate --force
+
+php artisan serve 
